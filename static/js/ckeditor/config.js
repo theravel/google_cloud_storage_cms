a079cae163592cf45f787261896264e07ef65464
Dmitry Tarasov <dimitry@blackopsdev.com>
2014-03-31 18:36:55 +0400

    Allow any type of content

--- static/js/ckeditor/config.js
+++ static/js/ckeditor/config.js
@@ -35,4 +35,7 @@ CKEDITOR.editorConfig = function( config ) {
 
 	// Make dialogs simpler.
 	config.removeDialogTabs = 'image:advanced;link:advanced';
+
+        // allow any type of content
+        config.allowedContent = true;
 };
