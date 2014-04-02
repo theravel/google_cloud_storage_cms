9e096be87c24d09fa3bfc44c308c29e879061bee
Dmitry Tarasov <dimitry@blackopsdev.com>
2014-04-02 12:17:49 +0400

    Add upload settings stub

--- config/ui.module.php
+++ config/ui.module.php
@@ -14,5 +14,22 @@ return array(
         'files_dir' => 'files',
         'gcs_bucket_name' => 'premium-modem-518.appspot.com',
         'suffix' => '.txt',
+        'upload' => array(
+            'allowed_extensions' => array(
+                'jpg',
+                'jpeg',
+                'gif',
+                'png',
+                'doc',
+                'docx',
+                'xls',
+                'xlsx',
+                'pdf',
+                'txt',
+            ),
+            'allowed_types' => array(
+                '',
+            ),
+        ),
     ),
 );
