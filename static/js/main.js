859f6e5c771858c575e16c27bfa639d13d407117
dtarasov <dtarasov@dataart.com>
2014-04-25 22:22:09 +0400

    Improve menu collapsing, remove logo hard sizing

--- static/js/main.js
+++ static/js/main.js
@@ -1,9 +1,9 @@
-$(function(){
-    $('.nav li')
-        .on('mouseenter', function() {
-            $(this).find('> .submenu').fadeIn(300);
-        })
-        .on('mouseleave', function() {
-            $(this).find('> .submenu').fadeOut(300);
-        });
+$(function() {
+    $('.nav li').on('mouseenter mouseleave', function() {
+        var element = $(this).find('> .submenu');
+        element.removeClass('right').fadeToggle(300);
+        if (element.length && element.offset().left + element.width() > $(window).width()) {
+            element.addClass('right');
+        }
+    });
 });
